e57b2c2719c53cf1c1bf4f2b71449313cfdd36d1
ThiagoHen2253 <thiagrique885@gmail.com>
2025-10-17 21:29:52 -0300

    Add files via upload

---
+++ aula17.10/js/meuscript.js
@@ -0,0 +1,10 @@
+function exibir(){
+    let paragrafo = document.getElementById("nome");
+    console.log(paragrafo);
+    paragrafo.innerHTML = "Exibindo Nome!!!!!!!!!!!!!"
+}
+function ocultar(){
+    let paragrafo = document.getElementById("nome");
+    console.log(paragrafo);
+    paragrafo.innerHTML = ""
+}
